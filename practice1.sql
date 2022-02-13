82b2d9c5ac26a5ebf23957d9ef28b31232efccb2
Pranav kumar <sanupranav@outlook.com>
2022-02-13 15:04:04 +0530

    feat: completed first mathematical tutorial with select statement

---
+++ practice1.sql
@@ -0,0 +1,16 @@
+SELECT 1+1 AS Result; 
+GO
+SELECT 1*1 AS Result; 
+GO
+SELECT 1/0 AS Result; 
+SELECT 1/1 AS Result; 
+GO
+-- Practice Activity I--
+--1. What is 4 plus 9? Please call the column MyAnswer.--
+SELECT 4+9 As MyAnswer
+--2. What is 15 minus 26? Please call the column Balance.--
+SELECT 15-26 As Balance
+--What is 24 times 4 plus 3? Please call the column MyResponse.--
+SELECT 24*4+3 As MyResponse
+-- What is 48 divided by 4? Please call the column Result.--
+SELECt 48/4 As Result
